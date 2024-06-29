b337569be0a2b0995fb496539d9c0f1944ffeb9c
Jeffrey L. Salvino II <jeffrey.salvino@student.saedubai.com>
2024-06-29 18:34:59 +0400

    Generator script and base enemy script
    I deleted the base enemy script and I made a function in the generator script that would spawn

--- Assets/Scripts/enemyScript/bRat.cs
+++ Assets/Scripts/enemyScript/bRat.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class bEnemyScript : MonoBehaviour
+public class bRat : MonoBehaviour
 {
     // Start is called before the first frame update
     void Start()
--- Assets/Scripts/generator.cs
+++ Assets/Scripts/generator.cs
@@ -7,15 +7,28 @@ using UnityEngine;
 
 public class generator : MonoBehaviour
 {
-    
-    void Start()
-    {
-        
-    }
+    float timer = 1;
+    public GameObject[] gObj;
 
-    // Update is called once per frame
-    void Update()
+    void FixedUpdate()
     {
-        
+        if (timer > 0)
+        {
+            timer -= Time.deltaTime;
+
+        }
+        else
+        {
+            
+            int chance = Random.Range(1, 101);
+            float pos_x = Random.Range(-4.0f, 4.0f);
+
+            if (chance <= 20)
+            {
+                Instantiate(gObj[0], new Vector3(pos_x, 6.0f, 0.1f), new Quaternion(0, 0, 0, 0));
+            }
+            timer = 2f;// Interval of the spawn timer
+        }
     }
 }
+
