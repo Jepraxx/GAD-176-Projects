809dacf9c29cc8eb15cf55564c9c034d3746ef5b
Jeffrey L. Salvino II <jeffrey.salvino@student.saedubai.com>
2024-06-29 17:17:00 +0400

    Added player
    For now I implimented a simple movement script

---
+++ Assets/Scripts/playerScript.cs
@@ -0,0 +1,32 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+/// <summary>
+/// This script is responsible for moving the character in the game it will have two functions
+/// update will handle the user input, while the fixed update will be the responsible for moving
+/// the actual player
+/// </summary>
+public class playerScript : MonoBehaviour
+{
+    //Make this visible in the inspector for easier fine tuning later on
+    [SerializeField] private float mSpeed = 10f;
+
+    public Rigidbody2D rBody2D;
+
+    Vector2 mveMent;
+
+
+    void Update()
+    {
+        mveMent.x = Input.GetAxisRaw("Horizontal");
+        mveMent.y = Input.GetAxisRaw("Vertical");
+    }
+
+    private void FixedUpdate()
+    {
+        rBody2D.MovePosition(rBody2D.position + mveMent * mSpeed * Time.deltaTime);
+    }
+}
+
+
+
