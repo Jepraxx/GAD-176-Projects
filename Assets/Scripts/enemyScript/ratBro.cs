08814ee8513f0504dc07f7e0dcb3f6338750f311
Jeffrey L. Salvino II <jeffrey.salvino@student.saedubai.com>
2024-06-30 18:39:11 +0400

    Added another behavior and functions and end scene
    I added a new behavior for rat boy and rat sis rat boy will get faster as he collides with the player and rat sis will end the game if she collides with the player and her script is responsible for loading the game over scene, in the game over scene it also has text telling the user the  game is over

--- Assets/Scripts/enemyScript/ratBro.cs
+++ Assets/Scripts/enemyScript/ratBro.cs
@@ -2,6 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using Unity.VisualScripting;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 /// <summary>
 /// This script is entirely dedicated for ratBro and this 
 /// script would inherit from the base rat script and would override
@@ -19,11 +20,27 @@ public class ratBro : bRat
     
     protected override void Move()
     {
+
         if (target != null && Vector2.Distance(transform.position, target.position) < distance)
         {
             //ratBro moves towards the player faster than the base class
             float modifiedSpeed = moveSpeed * 1.5f;
             transform.position = Vector2.MoveTowards(transform.position, target.position, modifiedSpeed * Time.deltaTime);
         }
+    }
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.CompareTag("Player"))
+        {
+            GetFaster();
+        }
+    }
+
+   //This lets Ratboy get faster when it collides with the player
+    private void GetFaster()
+    {
+        moveSpeed += 2f;
+        Debug.Log("RAT BOY IS GETTING FASTER");
+
     }
 }
--- Assets/Scripts/enemyScript/ratSis.cs
+++ Assets/Scripts/enemyScript/ratSis.cs
@@ -2,6 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using Unity.VisualScripting;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 /// <summary>
 /// This script is entirely dedicated for ratSis and this 
 /// script would inherit from the base rat script and would override
@@ -24,10 +25,26 @@ public class ratSis : bRat
         {
             if (target != null && Vector2.Distance(transform.position, target.position) < distance)
             {
-                //ratSis moves towards the player faster than the base class and ratBro
+                //ratSis moves towards the player faster than the base class and ratBro since the movespeed is modded and  multiplied by 5
                 float modifiedSpeed = moveSpeed * 5f;
                 transform.position = Vector2.MoveTowards(transform.position, target.position, modifiedSpeed * Time.deltaTime);
-            }
+            }                                   
         }
     }
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.CompareTag("Player"))
+        {
+            StopGame();
+        }
+    }
+
+    // Stops the entire game
+    private void StopGame()
+    {
+        SceneManager.LoadScene(1);
+        Debug.Log("Game Over! ratSis got the player!");
+        
+    }
 }
+
